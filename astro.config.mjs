b64a0cdb7481bf250946fb4097dce12fbd8db66e
unihous <167438973+unihous@users.noreply.github.com>
2024-04-20 17:02:48 +0000

    imported all geschichte images + initiated badminton

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import tunnel from "astro-tunnel";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  integrations: [tailwind(), tunnel()]
 });
