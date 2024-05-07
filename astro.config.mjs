4346860c60fb0dd8e8f94c6a23b7dad8cda33f2c
houstropics <165116600+houstropics@users.noreply.github.com>
2024-05-07 16:22:21 +0000

    added all sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import tunnel from "astro-tunnel";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), tunnel()]
+  integrations: [tailwind()]
 });
